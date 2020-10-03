54bb0671266af9e091b65cd9d235c69fcb71a00c
Cyril <effarek@gmail.com>
2020-10-03 23:07:29 +0200

    ADDED Particles on Wormhole : particles activate when the player is close.

--- Assets/Scripts/Wormhole_Behavior.cs
+++ Assets/Scripts/Wormhole_Behavior.cs
@@ -8,16 +8,42 @@ public class Wormhole_Behavior : MonoBehaviour
     public float rotation = 50f;
 
     private List<GameObject> travellers;
+    private ParticleSystem system;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
         travellers = new List<GameObject>();
+
+        system = GetComponent<ParticleSystem>();
+
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.forward * rotation * Time.deltaTime);
+
+        float distance = Vector2.Distance(player.transform.position, transform.position);
+
+        if (distance < 8f && distance > 0.5f)
+        {
+            if (!system.isPlaying)
+            {
+                system.Play();
+            }
+
+            ParticleSystem.EmissionModule emissionModule = system.emission;
+            emissionModule.rateOverTime = (int)(200 / distance);
+        }
+        else
+        {
+            if (system.isPlaying)
+            {
+                system.Stop();
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
