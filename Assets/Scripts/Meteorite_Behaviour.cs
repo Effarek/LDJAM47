1b45f3e19d4fb89f63eaf6d979c53c529cf68a04
GuillaumeRusso <guillaumerusso.info@gmail.com>
2020-10-03 14:59:52 +0200

    Meteorite & Spawner

---
+++ Assets/Scripts/Meteorite_Behaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteorite_Behaviour : MonoBehaviour
+{
+    public float Speed = 25;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.right * Speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Spawner_Bahaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner_Bahaviour : MonoBehaviour
+{
+    public float timerDuration = 5.0f;
+    private float timer = 0.0f;
+    private bool spanwed = false;
+
+    public GameObject objectToSpawn;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer < timerDuration)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            GameObject meteoriteCreated = Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+            meteoriteCreated.transform.rotation = transform.rotation;
+            timer = 0;
+        }
+    }
+}
