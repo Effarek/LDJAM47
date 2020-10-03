80fb4e392de249c9afb5bae7a34003c55de7bcfb
GuillaumeRusso <guillaumerusso.info@gmail.com>
2020-10-03 18:14:47 +0200

    Camera and Assets

---
+++ Assets/Scripts/Camera_Behaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Behaviour : MonoBehaviour
+{
+    public Vector3 cameraOffset = new Vector3(0, 0, 10);
+    public GameObject Player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Player.transform.parent.position - cameraOffset;
+    }
+}
