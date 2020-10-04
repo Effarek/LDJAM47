e0b7c8338e173c0f2966a896781deea575e5a4cc
GuillaumeRusso <guillaumerusso.info@gmail.com>
2020-10-04 19:12:48 +0200

    Modified meteorite destruction

--- Assets/Scripts/Meteorite_Behaviour.cs
+++ Assets/Scripts/Meteorite_Behaviour.cs
@@ -7,18 +7,18 @@ public class Meteorite_Behaviour : MonoBehaviour
     public float speed = 25.0f;
     public float maxDistance = 200.0f;
     private float currentDistance = 0f;
-    public GameObject LevelCenter;
+    public GameObject player;
 
     void Start()
     {
-        LevelCenter = GameObject.FindGameObjectWithTag("LevelCenter");   
+        player = GameObject.FindGameObjectWithTag("Player");   
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += transform.right * speed * Time.deltaTime;
-        currentDistance = Vector3.Distance(LevelCenter.transform.position, transform.position);
+        currentDistance = Vector3.Distance(player.transform.position, transform.position);
 
         if (currentDistance > maxDistance)
         {
